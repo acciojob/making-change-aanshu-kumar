e6e49a766bc13b9c6d1ef26d7f8a054814bc4d2d
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-26 17:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const makeChange = (c) => {
-  // your name here
-};
+    // your name here
+      let obj={
+          "q":0,
+          "d":0,
+          "n":0,
+          "p":0
+      }
+      while (c>0)
+          {
+              if(c>25)
+              {
+                  obj.q=Math.floor(c/25);
+                  c=c%25;
+              }
+              else if(c>10)
+              {
+                obj.d=Math.floor(c/10)
+                c=c%10;
+              }
+              else if(c>5)
+              {
+                obj.n= Math.floor(c/5);
+                c=c%5;
+              }
+              else{
+                obj.p=c;
+                c=0;
+              }
+          }
+          return obj;
+  };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
